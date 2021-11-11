a288eae864abc4d89225f3ab60f4025e460a87eb
Consuelo Ascanio <consueloascanio@gmail.com>
2021-11-11 14:25:57 -0300

    cors install

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv')
 dotenv.config({path:__dirname+'/.env'});
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
+app.use(cors());
 
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })

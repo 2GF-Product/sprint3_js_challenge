afbba41bf4ff46d4a697d77231c22ada289b581a
Arcy <arcysantoa@gmail.com>
2021-11-12 19:31:36 -0100

    add ListInvoice

--- web/src/App.js
+++ web/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import SignUp from "./components/SignUp";
 import SignIn from './components/SignIn';
+import formsInvoice from "./components/formsInvoice";
+import ListInvoice from "./components/ListInvoice";
 
 function App() {
   return (
---
+++ web/src/components/ListInvoice.js
@@ -0,0 +1,34 @@
+
+function ListInvoice(){
+
+    return(
+
+        <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Numero Factura</th>
+      <th>Nome Cliente</th>
+      <th>Açoes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    
+  </tbody>
+</Table>
+    )
+}
+
+export default ListInvoice

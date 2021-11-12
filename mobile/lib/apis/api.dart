b786eb79b174e50de92dbb767c479522aa97b2db
LoureiroPedro <pedroloureiro087@gmail.com>
2021-11-12 12:23:46 +0000

    Api configuration start

--- mobile/lib/apis/api.dart
+++ mobile/lib/apis/api.dart
@@ -1,4 +1,8 @@
+//const String ROOT="http://tutionteacher.rrtutors.com";
+//const String REGISTRATION="$ROOT/api/registration.php";
+//const String LOGIN="$ROOT/api/login.php";
+import 'package:http/http.dart' as http;
 
-const String ROOT="http://tutionteacher.rrtutors.com";
-const String REGISTRATION="$ROOT/api/registration.php";
-const String LOGIN="$ROOT/api/login.php";
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+}

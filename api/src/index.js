2cdeffd194e8c1dafa0c93f124fba40111d8fe26
Luis-Miguel1 <luismtb2016@gmail>
2021-11-15 01:48:54 -0100

    validate filed in signIn form

--- api/src/index.js
+++ api/src/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 
 
- mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
